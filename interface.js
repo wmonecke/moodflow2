9c6dd841fd666e4ef48eb277c9960e00a8579dcf
Walter Monecke <wmonecke@gmail.com>
2017-05-19 14:07:22 +0200

    Finished login flow and handler that initializes when tap is created

--- interface.js
+++ interface.js
@@ -1,38 +1,52 @@
 // ------------------------------ BOOTSTRAP -----------------------------------
-function bootstrapApp() {}
+var user;
+// start app handler
+bootstrapApp();
+function bootstrapApp() {
+    var savedUser;
+    chrome.storage.sync.get(savedUser, function(foundUser) {
+        if (foundUser.name === undefined || '') {
+            $('.logo, .date, footer, section.dashboard').css('display', 'none');
+            $('section.welcomeMessage, .darkenBackground').css('display', 'flex');
+            return;
+        }
+
+        user = foundUser;
+        fillInformation();
+        return;
+    });
+}
 // --------------------------- DEFINING USER -----------------------------------
-function User(name,
+function User(
+    name,
 	email,
-	gender,
-	timezone,
-	myLocation
+	gender
+	//timezone, // once the user is saved send user to signup if there is non existent
+    // and get location;
+	//myLocation
     ) {
 	this.name = name;
 	this.email = email;
 	this.gender = gender;
-	this.timezone = timezone;
-	this.location = myLocation;
+	this.timezone = null;
+	this.location = null;
 	this.favoriteVideos = [];
 	this.favoriteBackgrounds = [];
 	this.id = null; // null until user goes premium
 	this.searchbar = true;
 	this.todolist = true;
-	this.firstTimeLogin = true;
 	this.firstTimeMeditating = true;
 }
-User.prototype.searchbarON = () => {
+User.prototype.searchbarON  = () => {
 	this.searchbar = true;
 };
 User.prototype.searchbarOFF = () => {
 	this.searchbar = false;
 };
-User.prototype.hasLoggedIn = () => {
-	this.firstTimeLogin = false;
-};
 User.prototype.hasMeditated = () => {
 	this.firstTimeMeditating = false;
 };
-let name, email, gender, timezone, myLocation;
+let _name, _email, _gender, _timezone, _myLocation; // var defined by user during form input
 // ------------------------------ FUNCTIONS -----------------------------------
 function getBase64FromImageUrlAndSave(url) {
 	var img = new Image();
@@ -126,6 +140,9 @@ function checkEmailLength() {
 		return;
 	}
 }
+function fillInformation() { // called after getting user
+    $('.greeting').html(`Good day, ${user.name}.`);
+}
 // ----------------------------- ASYNC AJAX -----------------------------------
 // background image AJAX
 $.ajax({
@@ -157,7 +174,8 @@ $(document).ready(() => {
 	// on init: body and background fade in
 	$('body, .background').fadeIn(1000);
 
-	// google searchbar animation and palceholder logic
+// ------------------------ DASHBOARD LOGIC -----------------------------------
+	// ***HEADER*** google searchbar animation and palceholder logic
 	$('#placeholder').on('click', () => {
 		$('.ion-ios-search-strong').css('transform', 'translateX(300px)');
 
@@ -180,25 +198,48 @@ $(document).ready(() => {
 		}, 310);
 	});
 
-	// date logic and format
+	// ***HEADER*** date logic and format
 	let day = moment().format('dddd'); // monday
 	let monthDaynum = moment().format('MMMM Do'); // May 15th
 	$('.date').html(`${day}, ${monthDaynum}`);
 
-	// first time login message fade out and calling
-	$('.darkenBackground, #firstTimeClose').on('click', () => {
-		$('.darkenBackground, .welcomeMessage').fadeOut(300);
-		// call function that updates user heres
-	});
+    // ***MAIN***
 
-	// logout popup fadeIn and fadeOut
-	$('#logoutFadeIn').on('click', () => {
-		$('.logoutpopup').fadeIn(300);
+    // *** MOTIVATE LOGIC***
+    $('#motivateButton').on('click', () => {
+        $('.myiframe, .closeiframe').fadeIn(400);
+    });
+    $('.closeiframe').on('click', () => {
+        $('.closeiframe, .myiframe').fadeOut(500);
+    });
 
-		$('.logoutpopup').mouseleave(() => {
-			$('.logoutpopup').fadeOut(300);
-		});
-	});
+
+
+
+
+    // ***FOOTER*** logout popup fadeIn and fadeOut
+    $('#logoutFadeIn').on('click', () => {
+        $('.logoutpopup').fadeIn(300);
+
+        $('.logoutpopup').mouseleave(() => {
+            $('.logoutpopup').fadeOut(300);
+        });
+    });
+
+// ---------------------------- LOGIN LOGIC -----------------------------------
+    // first time login message fade out and calling
+    $('.darkenBackground, #firstTimeClose').on('click', () => {
+        $('.darkenBackground, .welcomeMessage').fadeOut(500, () => {
+            $('section.loginForm').fadeIn(500);
+        });
+
+    });
+
+    // set background-color of checkboxes
+    $('.control_indicator').on('click', function() {
+        $(this).css('background-color', '#ffd700');
+        $('.control-checkbox .control_indicator:after').css('display', 'block');
+    });
 
 	// preventDefault of form submition
 	$('.welcomeForm').submit((e) => {
@@ -211,18 +252,18 @@ $(document).ready(() => {
 
 		// if user presses 'Enter' and the input is not empty
 		if (e.which == 13 && input !== '') {
-			name = $('.usernameInput').val();
+			_name = $('.usernameInput').val();
 			//used only to greet the user when asking for the email
-			nameGreeting = name.toLowerCase();
+			nameGreeting = _name.toLowerCase();
 			$('.askEmail').html(`oh hey, ${nameGreeting}. <br> what is your <span class="highlight">email</span>?`);
 			console.log(nameGreeting);
 			// making sure the first letter is uppercase
-			name = name.charAt(0).toUpperCase() + name.slice(1);
+			_name = _name.charAt(0).toUpperCase() + _name.slice(1);
 			console.log(name);
 			$('.usernameInput').css({'color': 'transparent', 'border-bottom-color': 'white'});
 			$('.askName, .usernameInput').fadeOut(500, () => {
 
-				$('.askEmail, .emailInput').fadeIn(600);
+				$('.askEmail, .emailInput').fadeIn(500);
 				setTimeout(() => {
 					$('.noEmail').fadeIn(500);
 				}, 1000);
@@ -234,15 +275,14 @@ $(document).ready(() => {
 	$('.emailInput').on('keyup', () => {
 		checkEmailLength();
 	});
-
     $('.emailInput').focus(function() {
-        let input = $(this).val();
-        setInterval(() => {
-            checkEmailLength();
-        }, 500);
+      let input = $(this).val();
+      setInterval(() => {
+          checkEmailLength();
+      }, 500);
     });
 
-	// check if email is valid using a regex and fade in dashboard.
+	// check if email is valid using a regex and fade in askGender.
 	$('.emailInput').keypress((e) => {
 		let input = $('.emailInput').val();
 
@@ -264,17 +304,24 @@ $(document).ready(() => {
             // if email is valid save it into var and fade out
             } else if (validatedEmail === true) {
 
-				email = $('.emailInput').val();
+				_email = $('.emailInput').val();
                 $('.askGender').html(`last question, ${nameGreeting}. <br> are you a <span class="highlight">guy</span> or a <span class="highlight">girl</span>?`);
 
+				$('.emailInput').css({'color': 'transparent', 'border-bottom-color': 'transparent'}).val('');
 				$('.askEmail, .emailInput, .noEmail').fadeOut(500, () => {
-                    $('.askGender, .guy, .girl').fadeIn(500);
+                    $('.askGender, .control_indicator').fadeIn(500, () => {
+                        $('.control_indicator').css('animation-play-state', 'running');
+                        setTimeout(() => {
+                            $('.noGender').fadeIn(500);
+                        }, 1500);
+                    });
                 });
 				return;
 			}
 
 
 		} else if (e.which == 13 && input === '') { // if empty shake
+
 			let animationEvent = whichAnimationEvent();
 			$("input.emailInput").addClass("animated shake");
 			$("input.emailInput").one(animationEvent,
@@ -286,12 +333,46 @@ $(document).ready(() => {
 		}
 	});
 	$('.noEmail').on('click', () => {
-		email = null;
+		_email = null;
 		$('.askGender').html(`last question, ${nameGreeting}. <br> are you a <span class="highlight">guy</span> or a <span class="highlight">girl</span>?`);
 
-    $('.askEmail, .emailInput, .noEmail').fadeOut(500, () => {
-        $('.askGender, .guy, .girl').fadeIn(500);
-    });
+        $('.askEmail, .emailInput, .noEmail').fadeOut(500, () => {
+            $('.askGender, .control_indicator').fadeIn(500, () => {
+                $('.control_indicator').css('animation-play-state', 'running');
+            });
+            setTimeout(() => {
+                $('.noGender').fadeIn(500);
+            }, 1500);
+        });
 	});
 
+    // defining user gender depending on click
+    $('.noGender').on('click', () => {
+        _gender = null;
+    });
+    $('.guycheckbox').on('click', () => {
+        _gender = 'male';
+    });
+    $('.girlcheckbox').on('click', () => {
+        _gender = 'female';
+    });
+
+    // event after user choses gender or does not provide information
+    $('.afterGender').on('click', () => {
+
+        var savedUser = new User(_name, _email, _gender);
+
+        chrome.storage.sync.set(savedUser, function() {
+            console.log('user was saved');
+
+            chrome.storage.sync.get(savedUser, function(foundUser) {
+                user = foundUser;
+                fillInformation();
+                $('section.loginForm').fadeOut(500, () => {
+                    $('footer, .date, .logo, section.dashboard').fadeIn(2000);
+                });
+                return;
+            });
+        });
+    });
 });
